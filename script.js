48f82dce0bf8525b4fe0f9e152ddcfaf6dc70064
Skymmel <65237069+Skymmel@users.noreply.github.com>
2023-02-12 16:20:33 +0100

    .

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let navigationBar = document.getElementById("navigation-bar");
 
 function activeNav(){
-    if (!navigationBar.classList.contains("open")) {
-        navigationBar.classList.add("open")
-    } else {
+    if (navigationBar.classList.contains("open")) {
         navigationBar.classList.remove("open")
+
+    } else {
+        navigationBar.classList.add("open")
     }
 }

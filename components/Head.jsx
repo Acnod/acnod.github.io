7b6bcb573829f4bd081995fa6f1e8da78a63b2d2
Jakooob <sokoljakub14@gmail.com>
2023-04-14 18:39:24 +0200

    Open Graph in Head

--- components/Head.jsx
+++ components/Head.jsx
@@ -5,9 +5,11 @@ export default function DHead({ title }) {
         <>
             <Head>
                 <title>{title}</title>
-                <meta name="description" content="Acnod Team" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="shortcut icon" href="/brand/favicon.svg" type="image/x-icon" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="/brand/small-logo.png" />
+                <meta name="description" content="Acnod Team is a team of 2 developers that build sites." />
             </Head>
         </>
     )
